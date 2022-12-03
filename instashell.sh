90355c2e77b0e6c7a0a02c0d053a132adf5c0316
Ananya <90745373+anunicn47@users.noreply.github.com>
2022-12-03 09:10:31 +0000

    Update instashell.sh

--- instashell.sh
+++ instashell.sh
@@ -43,14 +43,14 @@ fi
 
 banner() {
 
-printf "\e[1;92m     _                                   _             _  _           \e[0m\n"
-printf "\e[1;92m _  | |                _                | |           | || |          \e[0m\n"
-printf "\e[1;92m( \ | | ____    ___  _| |_  _____   ___ | |__   _____ | || |          \e[0m\n"
-printf "\e[1;92m ) )| ||  _ \  /___)(_   _)(____ | /___)|  _ \ | ___ || || |          \e[0m\n"
-printf "\e[1;77m(_/ | || | | ||___ |  | |_ / ___ ||___ || | | || ____|| || |  _____   \e[0m\n"
-printf "\e[1;77m    |_||_| |_|(___/    \__)\_____|(___/ |_| |_||_____) \_)\_)(_____)  \e[0m\n"
+printf "\e[1;92m     _                                                                \e[0m\n"
+printf "\e[1;92m    | |                _                                              \e[0m\n"
+printf "\e[1;92m    | | ____    ___  _| |_  _____                                     \e[0m\n"
+printf "\e[1;92m    | ||  _ \  /___)(_   _)(____ |                                    \e[0m\n"
+printf "\e[1;77m    | || | | ||___ |  | |_ / ___ |                                    \e[0m\n"
+printf "\e[1;77m    |_||_| |_|(___/    \__)\_____|                                    \e[0m\n"
 printf "\n"
-printf "\e[1;77m\e[45m  Instagram Brute Forcer v1.5.4 Author: thelinuxchoice (Github/IG)   \e[0m\n"
+printf "\e[1;77m\e[45m  Instagram Brute Forcer v1.5.4 Author: anonAELLY (Github/IG)   \e[0m\n"
 printf "\n"
 }
 
